5101d82b708d521eae2ba25839502e73e6a461be
wangwenbin131416 <wangwenbin@creality.com>
2021-11-03 16:57:12 +0800

    fix: 🐛 删除多余的代码
    Change-Id: I73f0da1c2e37c956ef3bfcc2b1c24975faddd1ed

--- stl_thumbnail.cpp
+++ stl_thumbnail.cpp
@@ -231,18 +231,9 @@ int STLThumbnail(const std::string& stl_file_path, const std::string& thumbnail_
 //    rasterConfig.viewDir = Vec3(-1.0f, -1.0f, -1.0f);
 //    rasterConfig.viewRight = Vec3(1.0f, -1.0f, 0.0f);
 
-    stl::Parser stl_parser;
-    stl_parser.setCalculateVolume(false);
     Mesh mesh;
-
     CaseTracer tracer("cxbin::LoadT");
     std::vector<trimesh::TriMesh*> meshs = cxbin::loadT(raster_config.input, &tracer);
-    if (0== meshs.size())
-    {
-		//std::cout << "mesh is null." << std::endl;
-		return -1;
-    }
-    
     for (trimesh::TriMesh* aMesh : meshs)
     {
 		for (size_t i = 0; i < aMesh->faces.size(); i++)
@@ -267,13 +258,6 @@ int STLThumbnail(const std::string& stl_file_path, const std::string& thumbnail_
             mesh.push_back(atriangle);
 		}
     }
-
-
-        if (stl_parser.parseFile(mesh, raster_config.input) != 0)
-        {
-    //        std::cout << "parse stl file " << rasterConfig.input << " failed." << std::endl;
-            return -1;
-        }
 
     if (0 == mesh.size())
     {
